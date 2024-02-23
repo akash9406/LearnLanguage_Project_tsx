88a5604ddf05e485307c889abbbade1867961b7f
akash9406 <90980359+akash9406@users.noreply.github.com>
2024-02-23 16:39:45 +0530

    Update features.ts

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -2,32 +2,48 @@ import axios from "axios";
 import { generate } from "random-words";
 import _ from "lodash";
 
-const generateMCQ = (meaning:{Text:string}[],
-  idx:number 
-  ): string[] => {
-
-  const correctAns: string = meaning[idx].Text;   
-  // An array with all words execpt for correct ans
- const  meaningExceptcorrect = meaning.filter(
-     (i) => i.Text !== correctAns
- )
- //Randomly genrating 3 element from incorrectArray
-  const incorrectOptions: string[] = _.sampleSize(meaningExceptcorrect,3).map(
-    (i) => i.Text);
-
-    const mcqOptions = _.shuffle([...incorrectOptions,correctAns])
-
-
-  return mcqOptions;
+interface LangType {
+  // Define the type for LangType if not already defined
+  // ...
 }
 
-export const translateWords = async (params: LangType) : Promise<WordType[]> => {
+interface FetchedDataType {
+  translations: { text: string }[];
+  // Add other properties based on your actual response structure
+  // ...
+}
+
+interface WordType {
+  word: string;
+  meaning: string;
+  options: string[];
+}
+
+const generateMCQ = (
+  meaning: { Text: string }[],
+  idx: number
+): string[] => {
+  const correctAns: string = meaning[idx].Text;
+  const meaningExceptcorrect = meaning.filter((i) => i.Text !== correctAns);
+  const incorrectOptions: string[] = _.sampleSize(
+    meaningExceptcorrect,
+    3
+  ).map((i) => i.Text);
+
+  const mcqOptions = _.shuffle([...incorrectOptions, correctAns]);
+
+  return mcqOptions;
+};
+
+export const translateWords = async (
+  params: LangType
+): Promise<WordType[]> => {
   try {
     const words = generate(8).map((i) => ({
       Text: i,
     }));
 
-    const transkey = import.meta.env.VITE_MICROSOFT_TRANS
+    const transkey = import.meta.env.VITE_MICROSOFT_TRANS;
 
     const response = await axios.post(
       "https://microsoft-translator-text.p.rapidapi.com/translate",
@@ -39,7 +55,6 @@ export const translateWords = async (params: LangType) : Promise<WordType[]> => 
           profanityAction: "NoAction",
           textType: "plain",
         },
-
         headers: {
           "content-type": "application/json",
           'X-RapidAPI-Key': transkey,
@@ -48,39 +63,37 @@ export const translateWords = async (params: LangType) : Promise<WordType[]> => 
       }
     );
 
-    const receive: FetchedDataType[] = response.data
-  
-   const arr: WordType[] = receive.map((i,idx)=>{
+    const receive: FetchedDataType[] = response.data;
 
-        const options: string[]= generateMCQ(words,idx);
-    return {
+    const arr: WordType[] = receive.map((i, idx) => {
+      const options: string[] = generateMCQ(words, idx) as string[]; // Add type assertion
+      return {
         word: i.translations[0].text,
-        meaning:words[idx].Text,
+        meaning: words[idx].Text,
         options,
-    }
-   })
-   return arr
+      };
+    });
 
-  }catch(error){
-  throw new Error("Some error")
+    return arr;
+  } catch (error) {
+    throw new Error("Some error");
   }
-} 
+};
 
-export const countMatchingElements  = (arr1: string[] , 
-arr2:string[]
-): number =>{
-  
-  if(arr1.length !== arr2.length) throw new Error("Array are  not equal")
-  
+export const countMatchingElements = (
+  arr1: string[],
+  arr2: string[]
+): number => {
+  if (arr1.length !== arr2.length) throw new Error("Array lengths are not equal");
+
   let matchingCount = 0;
 
-   for(let i = 0;i < arr1.length; i++){
-    if(arr1[i] === arr2[i])matchingCount++;
-   }
-    return matchingCount;
-}
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) matchingCount++;
+  }
 
-
+  return matchingCount;
+};
 
 export const fetchAudio = async (
   text: string,
