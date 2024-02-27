5cd6bbbcc3bde20523e2092fbc0b5c2d5c467218
Akash Malakar <AkashMalakar3.3@gmail.com>
2024-02-27 23:27:32 +0530

    tryinge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -35,11 +35,20 @@ const Profile = () => {
   });
 
   useEffect(() => {
+    const getCookie = (name: string): string | undefined => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop()?.split(";").shift();
+    };
+    const token = getCookie("token");
     async function fetchData() {
       try {
         const data = await axios.get(
           "https://learnlanguage-backend.onrender.com/api/data/storedata",
           {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
             withCredentials: true,
           }
         );
